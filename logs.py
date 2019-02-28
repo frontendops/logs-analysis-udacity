b5f0609bbff51edf33be80ac84adebe5ae96dd11
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-02-28 10:49:01 -0500

    grouping days and total errors on that day descend by errors.

--- logs.py
+++ logs.py
@@ -8,9 +8,9 @@ question2 = "Who are the most popular article authors of all time? \n"
 question3 = "On which days did more than 1 % of requests lead to errors? \n"
 
 # queries that get passed into db function
-query1 = "select title,count(title) as views from articles join log on log.path = concat('/article/',articles.slug) group by title order by views desc limit 3"
+query1 = "select title,count(title) as impressions from articles join log on log.path = concat('/article/',articles.slug) group by title order by impressions desc limit 3"
 
-query2 = "select name, count(name) as views from articles join authors on articles.author = authors.id join log on log.path = concat('/article/',articles.slug) group by authors.name order by views desc limit 4;"
+query2 = "select name, count(name) as impressions from articles join authors on articles.author = authors.id join log on log.path = concat('/article/',articles.slug) group by authors.name order by impressions desc limit 4;"
 
 query3 = "select * from articles"
 
@@ -48,4 +48,32 @@ print("\n")
                 on log.path = concat('/article/',articles.slug)
             group by authors.name
                 order by views desc limit 4;
+"""
+
+
+# for now get days where status is 404 not found
+# view for query 3
+"""
+create view dates as
+    select to_char(time, 'DD-MM-YYYY') as day, count(*) as visits
+    from log
+    group by day;
+"""
+
+"""
+create view errorDate as 
+    select to_char(time, 'DD-MM-YYYY') as day, count(*) as totalErrors
+    from log
+    where status = '404 NOT FOUND'
+    group by day;
+"""
+
+
+# full query 3
+"""
+select dates.day, totalErrors
+    from dates, errorDate
+       where dates.day = errorDate.day
+        group by dates.day, totalErrors
+        order by totalErrors desc;
 """
