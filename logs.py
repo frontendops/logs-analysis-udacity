f2819bbd147b89c11f1055c6fa7552360af5fd0a
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-02-27 20:27:38 -0500

    second query added and logged

--- logs.py
+++ logs.py
@@ -8,8 +8,10 @@ question2 = "Who are the most popular article authors of all time? \n"
 question3 = "On which days did more than 1 % of requests lead to errors? \n"
 
 # queries that get passed into db function
-query1 = "select title,count(title) as views from articles,log where log.path = concat('/article/',articles.slug) group by title order by views desc limit 3"
-query2 = "select * from authors"
+query1 = "select title,count(title) as views from articles join log on log.path = concat('/article/',articles.slug) group by title order by views desc limit 3"
+
+query2 = "select name, count(name) as views from articles join authors on articles.author = authors.id join log on log.path = concat('/article/',articles.slug) group by authors.name order by views desc limit 4;"
+
 query3 = "select * from articles"
 
 
@@ -30,5 +32,20 @@ print(question1)
 print(connectDB(query1))
 print("\n")
 
-# print(question2)
+print(question2)
+print(connectDB(query2))
+print("\n")
+
 # print(question3)
+
+# full query 2
+"""
+    select name, count(name) as views 
+        from articles
+            join authors
+            on articles.author = authors.id
+                join log
+                on log.path = concat('/article/',articles.slug)
+            group by authors.name
+                order by views desc limit 4;
+"""
